c54be1977f025d0a4da37ed4cadb5f02ead13e99
Felix Ospald <felix.ospald@gmail.com>
2018-10-22 17:15:35 +0200

    updated build script

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ cd "$BUILD_DIR" || exit
 cmake -DCMAKE_VERBOSE_MAKEFILE:BOOL=OFF "$@" ../.. || exit
 make -j || exit
 
+echo ""
 echo "Build successful!"
 echo "Please update your environmet variables, i.e.:"
 echo "export PATH=\$PATH:$FIBERGEN_DIR/bin"
